6964602afb6cc382e58430e3f080170e4571269e
tonyzx0816 <lizhongxian23@gmail.com>
2025-03-04 00:09:33 -0800

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    AZURE_OPENAI_API_KEY: process.env.AZURE_OPENAI_API_KEY,
+    AZURE_OPENAI_ENDPOINT: process.env.AZURE_OPENAI_ENDPOINT,
+    AZURE_OPENAI_MODEL: process.env.AZURE_OPENAI_MODEL
+  }
+};
 
 export default nextConfig;
